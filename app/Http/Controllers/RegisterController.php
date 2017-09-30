02a8f8c78b88196d225a599543fd32c772b42cd0
Renato Lopez <lopez.renato@gmail.com>
2017-09-30 18:12:57 -0500

    cambio password ramdonico

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -48,12 +48,15 @@ class RegisterController extends Controller
     	$ysqsm = $this->actualizarysqsm($request->order_id,$request->order_status);
     	
     	if($request->authorization_result == '00' && $request->order_status == 'Autorizado'){
+    		
+    		
+    		
     		$mce = $this->actualizarmce($request->order_id);
-    		//return Redirect::to('http://www.yosiquierosermaestro.com/aprobado/'.$request->order_id);
-    		return Redirect::to('http://yosiquierosermaestro.local/aprobado/'.$request->order_id);
+    		return Redirect::to('http://www.yosiquierosermaestro.com/aprobado/'.$request->order_id);
+    		//return Redirect::to('http://yosiquierosermaestro.local/aprobado/'.$request->order_id);
     	} else {
-    		//return Redirect::to('http://www.yosiquierosermaestro.com/reprobado/'.$request->order_id);
-    		return Redirect::to('http://yosiquierosermaestro.local/reprobado/'.$request->order_id);
+    		return Redirect::to('http://www.yosiquierosermaestro.com/reprobado/'.$request->order_id);
+    		//return Redirect::to('http://yosiquierosermaestro.local/reprobado/'.$request->order_id);
     	}    	
     }
 
@@ -108,13 +111,28 @@ class RegisterController extends Controller
     	return $order;
     }
     
+    function generateRandomString($length = 5) {
+    	$characters = '123456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+    	$charactersLength = strlen($characters);
+    	$randomString = '';
+    	for ($i = 0; $i < $length; $i++) {
+    		$randomString .= $characters[rand(0, $charactersLength - 1)];
+    	}
+    	return $randomString;
+    }
+    
     private function actualizarmce ($orderId)
     {
     	$datas = DB::connection('yosiquierosermaestro')->select('select persons.*, orders.product_description from persons inner join orders on orders.customer_id = persons.id  where code = ?', [$orderId]);
     	
     	foreach ($datas as $data){
     		$usuario = 'Aspirante_'.$orderId;
-    		$password = md5($usuario);
+    		
+    		$passwNE=$this->generateRandomString();
+    		$password=md5($passwNE);
+    		$order = DB::connection('yosiquierosermaestro')->update('update orders set password_ne = ? where code = ?', [$passwNE, $orderId]);
+    		
+    		
     		$user =  DB::connection('mecapacitoecuador')->insert('insert into TB_USUARIOS(usu_usuario,usu_password,usu_nombre,usu_apellido,usu_mail,usu_perfil) value (?,?,?,?,?,?)',[$usuario,$password,$data->customer_name,$data->customer_lastname,$data->customer_email,'Estudiante']);
     	}
     	return $user;
